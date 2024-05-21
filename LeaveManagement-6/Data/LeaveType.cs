e970fd90344fd5c61de8e88736d06b423068e733
Krishna Kanth P.J <krishnovatiwe@gmail.com>
2024-05-21 23:19:10 +0530

    Layout changes, Added Leave tables and changed Default identity user.

---
+++ LeaveManagement-6/Data/LeaveType.cs
@@ -0,0 +1,11 @@
+﻿namespace LeaveManagement_6.Data
+{
+    public class LeaveType : BaseEntity
+    {
+        /// <summary>
+        /// We can add LeaveTypeId/Id which will consider as primary key
+        /// </summary>
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
